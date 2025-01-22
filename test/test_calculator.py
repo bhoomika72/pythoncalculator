d142f0aedadba20d3dd9524bd4026a7e843af156
bhoomika72 <bhoomika.soni.79@gmail.com>
2025-01-22 12:58:06 +0530

    changes made

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,34 +1,24 @@
 # COMMAND ----------
-with open("/dbfs/FileStore/pythoncalculator/src/calculator.py") as f:
-    print(f.read())
-
-# COMMAND ----------
 import pytest
 import sys
 
-sys.path.append('/FileStore/pythoncalculator/src')
+sys.path.append('./src')
 
-# Import dynamically
-calculator = __import__('calculator')
+from calculator import *
 
 def test_add():
-    assert calculator.add(3, 5) == 8
-    assert calculator.add(-1, 1) == 0
+    assert add(3, 5) == 8
+    assert add(-1, 1) == 0
 
 def test_subtract():
-    assert calculator.subtract(10, 5) == 5
-    assert calculator.subtract(0, 5) == -5
+    assert subtract(10, 5) == 5
+    assert subtract(0, 5) == -5
 
 def test_multiply():
-    assert calculator.multiply(4, 3) == 12
-    assert calculator.multiply(-2, 3) == -6
+    assert multiply(4, 3) == 12
+    assert multiply(-2, 3) == -6
 
 def test_divide():
-    assert calculator.divide(10, 2) == 5
+    assert divide(10, 2) == 5
     with pytest.raises(ValueError):
-        calculator.divide(10, 0)
-
-
-# COMMAND ----------
-
-!pytest /dbfs/FileStore/pythoncalculator/test_calculator.py
+        divide(10, 0)
