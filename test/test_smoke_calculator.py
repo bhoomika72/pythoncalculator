565697d58a634845fea7f1b8b9759b567f39fb74
bhoomika72 <bhoomika.soni.79@gmail.com>
2025-01-23 11:21:00 +0530

    changes made

--- test/test_smoke_calculator.py
+++ test/test_smoke_calculator.py
@@ -1,4 +1,3 @@
-import pytest
 import sys
 import os
 
@@ -6,21 +5,18 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src'
 
 from src.calculator import *
 def test_smoke_add():
-    # Smoke test for add function
     result = add(1, 1)
     assert result == 2
 
-def test_smoke_subtract():
-    # Smoke test for subtract function
+def test_smoke_subtract()
     result = subtract(2, 1)
     assert result == 1
 
 def test_smoke_multiply():
-    # Smoke test for multiply function
     result = multiply(2, 2)
     assert result == 4
 
 def test_smoke_divide():
-    # Smoke test for divide function
+
     result = divide(4, 2)
     assert result == 2
